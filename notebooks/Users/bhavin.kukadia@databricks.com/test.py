c39b398606906c9d081d1b69d19d3029934a71cd
Bhavin <bhavin.z@gmail.com>
2019-09-04 20:40:26 -0400

    testing commit

---
+++ notebooks/Users/bhavin.kukadia@databricks.com/test.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %sh wget https://databricks.com/wp-content/themes/databricks/assets/images/header_logo_2x.png
+
+# COMMAND ----------
+
+# MAGIC %sh wget https://code.jquery.com/jquery-3.3.1.min.js
+
+# COMMAND ----------
+
+# MAGIC %sh wget https://pypi.org
+
+# COMMAND ----------
+
+# MAGIC %sh wget https://maven.org
+
+# COMMAND ----------
+
