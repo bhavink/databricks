ccf9d62f54d0a01337e0c736620970d9f8776ac6
subbuiyer77 <$ape0477X>
2020-04-17 17:33:58 -0500

    getting started...

--- cryptography/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='cryptography',
-      version='0.0.1',
-      description='encrypt and decrypt data using pyspark udf',
-      packages=['data_security'],
-      zip_safe=False)
--- spark-r-us/src/main/CmdExec.py
+++
@@ -1,3 +0,0 @@
-df = spark.read.format("json").load("/databricks-datasets/iot/iot_devices.json")
-result = df.toJSON().take(10)
-result
