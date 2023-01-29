620fbc53b1b0e676703eb30e9db23c9ba6c08882
bhavink <bhavin.z@gmail.com>
2023-01-29 14:56:55 -0500

    updated markdown and added TF scripts

--- gcpdb4u/templates/create-workspace-and-gke-sa.tf
+++ gcpdb4u/templates/create-workspace-and-gke-sa.tf
@@ -1,46 +1,74 @@
 /*
-service account used to create databriks workspace
-prior to running this script, make sure that this SA exists as a databricks accounts admin by visiting 
-https://accounts.gcp.databricks.com > User Management > Search for the SA email and verify its role
+
+There are 2 GCP Service Accounts involved. Let's call them SA-1 and SA-2.
+SA-1 = has ServiceAccountTokenCreator role on the GCP project where databricks workspace is created. 
+SA-1 impersonates databricks workspace creator SA aka SA-2.
+SA-2 has set of permissions to create the workspace. 
+SA-2 is added to databricks account console with account admin role i.e.
+visit https://accounts.gcp.databricks.com > User Management > Search for the SA email and verify its role
+SA-1 impersonates SA-2
+
 */
-variable "databricks_google_service_account" { default = "test-ws-sa@labs-byovpc-test.iam.gserviceaccount.com"}
 
 /* 
-account used by `gcloud auth login`
-you may want to run this command on your local system to make sure identity used by gcloud
-here's an example of identity used, this identity will be impersonated by the servie account
-this script will be executed as [servie account]. 
+Before youu begin please run the following command and make sure the right account is configured to use gcloud command.
+account used by `gcloud auth list`
 */
-variable "delegate_from" {default="bhavin.kukadia@databricks.com"}
 
-# databricks account id available on accounts console > clicked on the logged in user email (top right corner)
-variable "databricks_account_id" { default = "e11e38c5-xyz-821612" }
+# gcloud auth activate-service-account token-creator@labs-byovpc-test.iam.gserviceaccount.com -key-file=token-creator-sa-creds.json
+
+variable "databricks_delegate" {
+ description = "This is SA-1. Allow either user:user.name@example.com, group:deployers@example.com or serviceAccount:sa1@project.iam.gserviceaccount.com to impersonate created service account aka SA-2"
+ type        = list(string)
+ # example of using a user identity with owner role on the project or a service account with tokenCreator role.
+ default = ["user:bhavin.kukadia@databricks.com","serviceAccount:token-creator@labs-byovpc-test.iam.gserviceaccount.com"]
+ 
+
+}
 
 /*
 in case if you are NOT using a shared vpc than service and host project id will be same
 host project hosts the vpc used by databricks
 */
-variable "google_host_project_id" { default = "labs-byovpc-test" }
 
-# this is where databricks managed gke is created
-variable "google_service_project_id" { default = "labs-byovpc-test" }
+# TODO: All these variables has be set to the proper values
+variable "project" {
+  default = "labs-byovpc-test"
+}
+variable "host_project" {
+  default = "labs-byovpc-test"
+}
+variable "databricks_account_id" {
+  default = "e11e38c5-a449-47b9-b37f-0fa36c821612"
+}
+variable "databricks_workspace_provisioner_sa_alias" {
+  default = "databricks-workspace-creator"
+}
+variable "databricks_region" {
+  default = "us-central1"
+}
+variable "databricks_vpc_id" {
+  default = "tf-test-vpc"
+}
+variable "databricks_node_subnet" {
+  default = "tf-node-subnet"
+}
+variable "databricks_pod_subnet" {
+  default = "tf-pod-subnet"
+}
+variable "databricks_service_subnet" {
+  default = "tf-service-subnet"
+}
+variable "databricks_gke_master_ip_range" {
+  default = "10.39.0.0/28"
+}
 
-# in this example projectId and name are same, yours could be different
-variable "google_service_project_name" { default = "labs-byovpc-test" }
-
-variable "google_region" { default = "us-central1" }
-
-variable "vpc_id" {default = "tf-test-vpc"}
-variable "node_subnet" {default = "tf-node-subnet"}
-variable "pod_subnet" {default = "tf-pod-subnet"}
-variable "service_subnet" {default = "tf-service-subnet"}
-variable "gke_master_ip_range" {default = "10.39.0.0/28"}
 
 terraform {
   required_providers {
     databricks = {
       source = "databricks/databricks"
-      version = "1.8.0"
+      # version = "1.8.0"
     }
     google = {
       source  = "hashicorp/google"
@@ -48,32 +76,96 @@ terraform {
   }
 }
 
-provider "google" {
-    project = var.google_service_project_name
-    region  = var.google_region
-}
-
-provider "databricks" {
-  host                   = "https://accounts.gcp.databricks.com"
-  google_service_account = var.databricks_google_service_account
-}
-
 data "google_client_openid_userinfo" "me" {
+}
+
+# Service account for databricks workspace creation aka SA-2
+
+resource "google_service_account" "sa_databricks_workspace_creator" {
+  project      = var.project
+  account_id   = "databricks-workspace-creator"
+  display_name = "Service Account For Databricks Workspace Creation"
+}
+
+output "databricks_service_account" {
+  value       = google_service_account.sa_databricks_workspace_creator.email
+  description = "Add this email as a user in the Databricks account console"
+}
+
+data "google_iam_policy" "databricks_sa_token_creator" {
+  binding {
+    role    = "roles/iam.serviceAccountTokenCreator"
+    members = var.databricks_delegate
+  }
+}
+
+resource "google_service_account_iam_policy" "databricks_sa_impersonator" {
+  service_account_id = google_service_account.sa_databricks_workspace_creator.name
+  policy_data        = data.google_iam_policy.databricks_sa_token_creator.policy_data
+  depends_on         = [google_service_account.sa_databricks_workspace_creator]
+}
+
+resource "google_project_iam_custom_role" "databricks_workspace_creator" {
+  project = var.project
+  role_id = "databricksWorkspaceCreator"
+  title   = "Databricks Workspace Creator"
+  permissions = [
+    "iam.serviceAccounts.getIamPolicy",
+    "iam.serviceAccounts.setIamPolicy",
+    "iam.roles.create",
+    "iam.roles.delete",
+    "iam.roles.get",
+    "iam.roles.update",
+    "resourcemanager.projects.get",
+    "resourcemanager.projects.getIamPolicy",
+    "resourcemanager.projects.setIamPolicy",
+    "serviceusage.services.get",
+    "serviceusage.services.list",
+    "serviceusage.services.enable"
+  ]
+}
+
+data "google_client_config" "current" {}
+
+output "custom_role_url" {
+  value = "https://console.cloud.google.com/iam-admin/roles/details/projects%3C${data.google_client_config.current.project}%3Croles%3C${google_project_iam_custom_role.databricks_workspace_creator.role_id}"
+}
+
+resource "google_project_iam_member" "iam_member_databricks_workspace_creator" {
+  project = var.project
+  role    = google_project_iam_custom_role.databricks_workspace_creator.id
+  member  = "serviceAccount:${google_service_account.sa_databricks_workspace_creator.email}"
+  depends_on = [
+    google_project_iam_custom_role.databricks_workspace_creator,
+    google_service_account.sa_databricks_workspace_creator
+  ]
+}
+
+# Applicable only if you are using a shared vpc where VPC resides in host project and GKE in service project 
+resource "google_project_iam_member" "host_project_role" {
+  project 	= var.host_project
+  role 		= "roles/compute.networkUser"
+  member 	= "serviceAccount:${google_service_account.sa_databricks_workspace_creator.email}"
 }
 
 
 resource "google_service_account" "databricks" {
     account_id   = "databricks" #need to use "databricks"
     display_name = "Databricks SA for GKE nodes"
+    project = var.project
 }
 output "service_account" {
     value       = google_service_account.databricks.email
-    description = "Add this email as a user in the Databricks account console"
+    description = "Default SA for GKE nodes"
 }
-# create this role if GKE and VPC resides in same GCP project
+
+# minimum persmissions requied to propagate tags set at databricks cluster level to underlying GCP resources.
+# this role will be assigned to an SA which is then used by GKE as a nodeServiceAccount instead of using the default compute engine
+
 resource "google_project_iam_custom_role" "databricks_gke_custom_role" {
     role_id = "databricks_default_gke_compute"
     title   = "Databricks SA for VMs"
+    project = var.project
     permissions = [
     "compute.disks.get",
     "compute.disks.setLabels",
@@ -82,15 +174,16 @@ resource "google_project_iam_custom_role" "databricks_gke_custom_role" {
     ]
 }
 
-# assign custom role to the databricks gke
+# assign custom role to the gke default SA
 resource "google_project_iam_member" "databricks_gke_custom_role" {
     role   = google_project_iam_custom_role.databricks_gke_custom_role.id
-    project = "${var.google_service_project_id}"
+    project = "${var.project}"
     member = "serviceAccount:${google_service_account.databricks.email}"
 }
 
+# assign role to the gke default SA
 resource "google_project_iam_binding" "databricks_gke_node_role" {
-  project = "${var.google_service_project_id}"
+  project = "${var.project}"
   role = "roles/container.nodeServiceAccount"
   members = [
     "serviceAccount:${google_service_account.databricks.email}"
@@ -99,166 +192,116 @@ resource "google_project_iam_binding" "databricks_gke_node_role" {
 
 
 
-resource "random_string" "suffix" {
+# Initialize provider in "accounts" mode to provision new workspace
+provider "databricks" {
+  alias                  = "accounts"
+  host                   = "https://accounts.gcp.databricks.com"
+  google_service_account = "${var.databricks_workspace_provisioner_sa_alias}@${var.project}.iam.gserviceaccount.com"
+  account_id             = var.databricks_account_id
+}
+
+
+
+# Random suffix for databricks network and workspace
+resource "random_string" "databricks_suffix" {
   special = false
   upper   = false
-  length  = 6
-}
-resource "databricks_mws_networks" "this" {
-  account_id     = var.databricks_account_id
-  network_name = "bk-tf-nw-config${random_string.suffix.result}"
-  gcp_network_info {
-    network_project_id = var.google_host_project_id
-    vpc_id = var.vpc_id
-    subnet_id = var.node_subnet
-    subnet_region = var.google_region
-    pod_ip_range_name = var.pod_subnet
-    service_ip_range_name = var.service_subnet
-  }
+  length  = 2
 }
 
-resource "databricks_mws_workspaces" "this" {
+# Provision databricks network configuration
+resource "databricks_mws_networks" "databricks_network" {
+  provider     = databricks.accounts
   account_id   = var.databricks_account_id
-  network_id   = databricks_mws_networks.this.network_id 
-  workspace_name = "bk-tf-${random_string.suffix.result}"
-  location = var.google_region
-    gke_config {
-      connectivity_type = "PRIVATE_NODE_PUBLIC_MASTER"
-      master_ip_range = "${var.gke_master_ip_range}"
-    }
-    cloud_resource_container {
-    gcp {
-      project_id = var.google_service_project_id
-    }
+  # name needs to be of length 3-30 incuding [a-z,A-Z,-_]
+  network_name = "${var.project}-nw-${random_string.databricks_suffix.result}"
+  gcp_network_info {
+    network_project_id    = var.project
+    vpc_id                = var.databricks_vpc_id
+    subnet_id             = var.databricks_node_subnet
+    subnet_region         = var.databricks_region
+    pod_ip_range_name     = var.databricks_pod_subnet
+    service_ip_range_name = var.databricks_service_subnet
   }
 }
 
-  output "databricks_network" {
-  value = databricks_mws_networks.this.network_id
-}
-  output "databricks_host" {
-  value = databricks_mws_workspaces.this.workspace_url
+# Provision databricks workspace in a customer managed vpc
+# https://docs.gcp.databricks.com/administration-guide/account-settings-gcp/workspaces.html#create-a-workspace-using-the-account-console
+
+resource "databricks_mws_workspaces" "databricks_workspace" {
+  provider       = databricks.accounts
+  account_id     = var.databricks_account_id
+  workspace_name = "${var.project}-${random_string.databricks_suffix.result}"
+  location       = var.databricks_region
+
+  cloud_resource_container {
+    gcp {
+      project_id = var.project
+    }
+  }
+
+  network_id = databricks_mws_networks.databricks_network.network_id
+
+  gke_config {
+    connectivity_type = "PRIVATE_NODE_PUBLIC_MASTER"
+    master_ip_range   = var.databricks_gke_master_ip_range
+  }
 }
 
-/*
-Now, after you’ve declared a workspace, 
-you should provision the infrastructure within that workspace. 
-The following configuration declares the ability for your user principal 
-to login into this workspace, as at this point only the var.
-databricks_google_service_account can log into that workspace via API through OIDC token. 
-This also adds administrative access to your user, which you don’t have by default. 
-Please pay special attention to the fact that we’re using multiple instances of databricks
-provider within the same Terraform module and therefore each resource 
-block must include provider attribute to refer to workspace instance. 
-If you’re noticing “could not obtain OIDC token. impersonate: an audience 
-must be provided” errors, then it means that there’s no 
-value in databricks_mws_workspaces.this.workspace_url yet 
-and you have to apply this configuration either through different modules or apply steps.
-*/
+# Provision databricks workspace in a databricks managed vpc
+# https://docs.gcp.databricks.com/administration-guide/account-settings-gcp/workspaces.html#create-a-workspace-using-the-account-console
+
+# resource "databricks_mws_workspaces" "this" {
+#  provider       = databricks.accounts
+#  account_id     = var.databricks_account_id
+#  workspace_name = "bk-tf-demo1"
+#  location       = data.google_client_config.current.region
+
+
+#  cloud_resource_container {
+#     gcp {
+#       project_id = var.google_project
+#     }
+#   }
+# }
+
+
+# Output host url
+output "databricks_host" {
+  value = databricks_mws_workspaces.databricks_workspace.workspace_url
+}
+
+### Databricks Workspace Automation (End) ###
+
+
+### Add admin user to the workspace
 
 provider "databricks" {
  alias                  = "workspace"
- host                   = databricks_mws_workspaces.this.workspace_url
- google_service_account = var.databricks_google_service_account
+ host                   = databricks_mws_workspaces.databricks_workspace.workspace_url
+ google_service_account = google_service_account.sa_databricks_workspace_creator.email
 }
 
 
 data "databricks_group" "admins" {
- depends_on   = [databricks_mws_workspaces.this]
+ depends_on   = [databricks_mws_workspaces.databricks_workspace]
  provider     = databricks.workspace
  display_name = "admins"
 }
 
-
+# Adds user to the workspace
 resource "databricks_user" "me" {
- depends_on = [databricks_mws_workspaces.this]
-
-
+ depends_on = [databricks_mws_workspaces.databricks_workspace]
  provider  = databricks.workspace
- user_name = data.google_client_openid_userinfo.me.email
+ #user_name = data.google_client_openid_userinfo.me.data
+ user_name = "bhavin.kukadia@databricks.com"
 }
 
-
+# Assigns workspace admin role to the user added
 resource "databricks_group_member" "allow_me_to_login" {
- depends_on = [databricks_mws_workspaces.this]
+ depends_on = [databricks_mws_workspaces.databricks_workspace]
  
  provider  = databricks.workspace
  group_id  = data.databricks_group.admins.id
  member_id = databricks_user.me.id
-}
-
-
-/*
-Once your user access is defined, 
-you’ll want to declare a couple of related things within 
-workspace - a notebook in the home folder, smallest possible 
-Databricks cluster and a job that executes that notebook on that cluster.
-*/
-
-data "databricks_current_user" "me" {
- depends_on = [databricks_mws_workspaces.this]
-
-
- provider = databricks.workspace
-}
-
-
-data "databricks_spark_version" "latest" {
- depends_on   = [databricks_mws_workspaces.this]
- 
- provider = databricks.workspace
-}
-
-
-data "databricks_node_type" "smallest" {
- depends_on = [databricks_mws_workspaces.this]
- provider   = databricks.workspace
- local_disk = true
-}
-
-
-resource "databricks_notebook" "this" {
- depends_on = [databricks_mws_workspaces.this]
-
-
- provider = databricks.workspace
- path     = "${data.databricks_current_user.me.home}/Terraform"
- language = "PYTHON"
- content_base64 = base64encode(<<-EOT
-   # created from ${abspath(path.module)}
-   display(spark.range(10))
-   EOT
- )
-}
-
-
-resource "databricks_cluster" "this" {
- depends_on              = [databricks_mws_workspaces.this]
- provider                = databricks.workspace
- cluster_name            = "Shared Autoscaling  (by ${data.databricks_current_user.me.alphanumeric})"
- spark_version           = data.databricks_spark_version.latest.id
- node_type_id            = data.databricks_node_type.smallest.id
- autotermination_minutes = 20
- autoscale {
-   min_workers = 1
-   max_workers = 2
- }
-}
-
-resource "databricks_job" "this" {
- depends_on          = [databricks_mws_workspaces.this]
- provider            = databricks.workspace
- name                = "Terraform Demo (${data.databricks_current_user.me.alphanumeric})"
- existing_cluster_id = databricks_cluster.this.id
- notebook_task {
-   notebook_path = databricks_notebook.this.path
- }
-}
-
-output "notebook_url" {
- value = databricks_notebook.this.url
-}
-
-output "job_url" {
- value = databricks_job.this.url
 }
